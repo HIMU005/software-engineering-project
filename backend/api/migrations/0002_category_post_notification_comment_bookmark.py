30dc8e5ee1e156924a00e77f9307ded17cca8923
jobaer6768 <jobaer9898@gmail.com>
2025-05-13 23:10:11 +0600

    created model for notification, comment and bookmark

--- backend/api/migrations/0002_category_post_notification_comment_bookmark.py
+++ backend/api/migrations/0002_category_post_notification_comment_bookmark.py
@@ -41,4 +41,44 @@ class Migration(migrations.Migration):
                 'verbose_name_plural': 'Post',
             },
         ),
+        migrations.CreateModel(
+            name='Notification',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('type', models.CharField(choices=[('Like', 'Like'), ('Comment', 'Comment')], max_length=100)),
+                ('seen', models.BooleanField(default=False)),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.post')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name_plural': 'Notification',
+            },
+        ),
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('email', models.CharField(max_length=100)),
+                ('comment', models.TextField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.post')),
+            ],
+            options={
+                'verbose_name_plural': 'Comment',
+            },
+        ),
+        migrations.CreateModel(
+            name='Bookmark',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.post')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name_plural': 'Comment',
+            },
+        ),
     ]
